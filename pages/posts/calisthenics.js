3023eaaadd127eea313e420502a2ba9804f8a2a2
saunderscox <saunderscox@gmail.com>
2021-10-25 13:23:28 -0400

    updated calistehnics page

--- pages/posts/calisthenics.js
+++ pages/posts/calisthenics.js
@@ -11,18 +11,37 @@ export default function Calisthenics() {
       <Head>
         <title>Calisthenics</title>
       </Head>
-      <h1>Calisthenics</h1>
-      <p>
+      <h1>Calisthenics {props.peepee}</h1>
+
+      <h4>
         I first began my calisthenics journey through r/bodyweightfitness's{" "}
         <Link href={rrURL}>
-          <a>recommended routine.</a>
+          <a> recommended routine.</a>
         </Link>
+      </h4>
+
+      <h2>A Gymless Lifestyle</h2>
+      <p>
+        I grew up in two separate households, alternating every half a week.
+        Whenever it was time to go to the other house, I'd pack up my backpack
+        with everything I'd want to take with me. This led me to adapt a
+        minimalist lifestyle early on in my life.
       </p>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <p>
+        Fast forward to independent life, and it became such that anything that
+        I considered worth keeping was anything that I could travel with in my
+        car. I keep as few subscriptions as possible, I budget out my home
+        cooked meals, my phone home screen is simplified, and my fitness goals
+        exist to maintain good health.
+      </p>
+      <p>
+        I'd been a part of a gym through my dad's subscription, but after
+        completing school, I was on my own more, visiting friends that were
+        hundreds of miles apart. No subscription could address that nomad
+        lifestyle.
+      </p>
+
+      <h2></h2>
     </Layout>
   );
 }
